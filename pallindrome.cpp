49ee722ca6018e11cb3bfeece0662c52d85b26be
SagunPanda <pandasagun@gmail.com>
2022-08-09 21:13:23 +0530

    pallindrome number

---
+++ pallindrome.cpp
@@ -0,0 +1,27 @@
+//write a program to check weather a number is pallindrome or not
+//code
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n,rev=0,rem;
+    cout<<"enter a num: ";
+    cin>>n;
+    do
+    {
+        rem=n%10;
+        rev=10*rev+rem;
+        n=n/10;
+    }
+    while (n!=0);
+    {
+        cout<<rev;
+     }
+    if(n==rev)
+    cout<<"its a pallindrome num";
+    else
+    cout<<"not a pallindrome no";
+    
+        
+    
+}
