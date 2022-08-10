5f806eef33c23badf04d99ae0f7e0c0306b627f2
SagunPanda <pandasagun@gmail.com>
2022-08-10 20:45:49 +0530

    reverse number

---
+++ leapyear.cpp
@@ -0,0 +1,21 @@
+//write a program to check weather it is leap yaer or not!
+//code
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<< "enter the year";
+    int y;
+    cin>> y;
+    if((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+    {
+        cout<<"this is a leap year ";
+
+    }
+    else
+    {
+        cout<<"this is not a leap year";
+    }
+    return 0;
+}   
+
